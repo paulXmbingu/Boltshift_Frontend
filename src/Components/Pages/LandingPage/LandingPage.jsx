528f2667e59a063f962ecc8ea12a54885324fa21
Paul <paulXmbingu@gmail.com>
2024-05-12 12:37:38 +0300

    update

--- src/Components/Pages/LandingPage/LandingPage.jsx
+++ src/Components/Pages/LandingPage/LandingPage.jsx
@@ -1,16 +1,20 @@
 import HotDealTodaySection from "./HotDealTodaySection";
 import PopularCategoriesSection from "./PopularCategoriesSection";
 
+
+const pageStyle={
+    display: "flex",
+    minWidth: "var(--Grid-layouts-iPhone-375px, 375px)",
+    maxWidth: "1920px",
+    flexDirection: "column",
+    alignItems: "center",
+    alignSelf: "stretch",
+}
+
+
 function LandingPage() {
     return(
-        <div style={{
-            display: "flex",
-            minWidth: "var(--Grid-layouts-iPhone-375px, 375px)",
-            maxWidth: "1920px",
-            flexDirection: "column",
-            alignItems: "center",
-            alignSelf: "stretch",
-        }}>
+        <div style={pageStyle}>
             <PopularCategoriesSection />
             <HotDealTodaySection />
         </div>
