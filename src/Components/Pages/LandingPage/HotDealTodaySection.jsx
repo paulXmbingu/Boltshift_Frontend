a98a26a07d84d88bb907a9dd1452805b251765b3
Paul <paulXmbingu@gmail.com>
2024-05-13 08:28:30 +0300

    Update

--- src/Components/Pages/LandingPage/HotDealTodaySection.jsx
+++ src/Components/Pages/LandingPage/HotDealTodaySection.jsx
@@ -1,30 +1,33 @@
 import HotDealTodayCard from "../../Shared/HotDealTodayCard";
 import SectionHeader from "../../Shared/SectionHeader";
 
+const popCatSection={
+    display: "flex",
+    minWidth: "var(--Grid-layouts-iPhone-375px, 375px)",
+    maxWidth: "var(--grid-layouts-desktop-1280-px, 1280px)",
+    padding: "var(--spacing-system-guides-corner-radius-12, 48px) var(--spacing-system-guides-corner-radius-4, 16px)",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    gap: "var(--spacing-system-guides-corner-radius-10, 40px)",
+    alignSelf: "stretch",
+}
+
+const catCards={
+    display: "flex",
+    alignItems: "flex-start",
+    alignContent: "flex-start",
+    gap: "16px var(--spacing-system-guides-corner-radius-4, 16px)",
+    alignSelf: "stretch",
+    flexWrap: "wrap"
+}
 
 function HotDealTodaySection() {
     return (
-        <hotDealTodaySection style={{
-            display: "flex",
-            minWidth: "var(--Grid-layouts-iPhone-375px, 375px)",
-            maxWidth: "var(--grid-layouts-desktop-1280-px, 1280px)",
-            padding: "var(--spacing-system-guides-corner-radius-12, 48px) var(--spacing-system-guides-corner-radius-4, 16px)",
-            flexDirection: "column",
-            alignItems: "flex-start",
-            gap: "var(--spacing-system-guides-corner-radius-10, 40px)",
-            alignSelf: "stretch",
-        }} >
+        <hotDealTodaySection style={popCatSection} >
             <div> 
                 <SectionHeader text="Hot Deals Today" />
             </div>
-            <catCards style={{
-                display: "flex",
-                alignItems: "flex-start",
-                alignContent: "flex-start",
-                gap: "16px var(--spacing-system-guides-corner-radius-4, 16px)",
-                alignSelf: "stretch",
-                flexWrap: "wrap"
-            }}>
+            <catCards style={catCards}>
                 <HotDealTodayCard />
                 <HotDealTodayCard />
                 <HotDealTodayCard />
