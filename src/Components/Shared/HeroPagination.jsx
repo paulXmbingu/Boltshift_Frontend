29cf6fe59a9ac959ea10188fd07ebd65e581f443
Paul <paulXmbingu@gmail.com>
2024-05-14 21:44:09 +0300

    Update

--- src/Components/Shared/HeroPagination.jsx
+++ src/Components/Shared/HeroPagination.jsx
@@ -14,11 +14,29 @@ const pagination={
     backdropFilter: 'blur(4px)'    
 }
 
+const paginationDot={
+    width: '8px',
+    height: '8px',
+    borderRadius: 'var(--radius-full, 9999px)',
+    background: 'var(--Colors-Background-bg-quaternary, #EAECF0)'    
+}
+
+const paginationDotCurrent={
+    width: '40px',
+    height: '6px',
+    borderRadius: 'var(--radius-full, 9999px)',
+    background: 'var(--Colors-Foreground-fg-brand-primary_alt, #DA154D)'
+}
+
 //FUNCTIONALITY
 function HeroPagination() {
     return(
         <div style={pagination}>
-            Pagination Dots
+            <div style={paginationDot}></div>
+            <div style={paginationDotCurrent}></div>
+            <div style={paginationDot}></div>
+            <div style={paginationDot}></div>
+            <div style={paginationDot}></div>
         </div>
     );
 
