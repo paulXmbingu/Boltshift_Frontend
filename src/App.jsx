1eb96701c8576a0a97e089bca55081c2c9a5b58f
Paul <paulXmbingu@gmail.com>
2024-05-18 17:47:36 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import LandingPage from './Components/Pages/LandingPage/LandingPage'
+import InstallPrompt from './Components/InstallPrompt';
 
 function App() {
   return (
     <div>
+      <InstallPrompt />
       <LandingPage />
     </div>
   )
