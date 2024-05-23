56ede8ba090790b4c0926c33bf2e1e0d4a6c363a
Paul <paulXmbingu@gmail.com>
2024-05-23 17:05:24 +0300

    Update

--- src/Common/HeroPagination.jsx
+++ src/Common/HeroPagination.jsx
@@ -1,47 +1,16 @@
 import React from "react";
+import styles from './HeroPagination.module.css';
 
-//STYLING
-const pagination={
-    display: 'flex',
-    padding: 'var(--spacing-md, 8px)',
-    justifyContent: 'center',
-    alignItems: 'center',
-    gap: 'var(--spacing-lg, 12px)',
-    borderRadius: 'var(--radius-full, 9999px)',
-    background: 'var(--Component-colors-Alpha-alpha-white-90, rgba(255, 255, 255, 0.90))',
-    
-    /* Backdrop blurs/backdrop-blur-sm */
-    backdropFilter: 'blur(4px)'    
-}
-
-const paginationDot={
-    width: '8px',
-    height: '8px',
-    borderRadius: 'var(--radius-full, 9999px)',
-    background: 'var(--Colors-Background-bg-quaternary, #EAECF0)',
-    cursor: 'pointer',
-}
-
-const paginationDotCurrent={
-    width: '40px',
-    height: '6px',
-    borderRadius: 'var(--radius-full, 9999px)',
-    background: 'var(--Colors-Foreground-fg-brand-primary_alt, #DA154D)',
-    cursor: 'pointer',
-}
-
-//FUNCTIONALITY
 function HeroPagination() {
-    return(
-        <div style={pagination}>
-            <div style={paginationDot}></div>
-            <div style={paginationDotCurrent}></div>
-            <div style={paginationDot}></div>
-            <div style={paginationDot}></div>
-            <div style={paginationDot}></div>
+    return (
+        <div className={styles.pagination}>
+            <div className={styles.paginationDot}></div>
+            <div className={styles.paginationDotCurrent}></div>
+            <div className={styles.paginationDot}></div>
+            <div className={styles.paginationDot}></div>
+            <div className={styles.paginationDot}></div>
         </div>
     );
-
-};
+}
 
 export default HeroPagination;
