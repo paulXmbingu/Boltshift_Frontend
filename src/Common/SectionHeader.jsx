6c15522e75ccb32dcc90ffdf6933b9e61020fde5
Paul <paulXmbingu@gmail.com>
2024-05-23 16:52:17 +0300

    Update

--- src/Common/SectionHeader.jsx
+++ src/Common/SectionHeader.jsx
@@ -1,49 +1,15 @@
 import React from "react";
+import styles from "./SectionHeader.module.css";
 
-//STYLING
-const sectionHeaderStyles = {
-    container: {
-        display: 'flex',
-        minWidth: '336px',
-        maxWidth: '1280px',
-        alignItems: 'flex-start',
-        alignContent: 'flex-start',
-        gap: '4px',
-        alignSelf: 'stretch',
-        flexWrap: 'wrap',
-    },
-
-    image: {
-        width: '32px',
-        height: '32px',
-    },
-    
-    text: {
-        display: '-webkit-box',
-        WebkitBoxOrient: 'vertical',
-        WebkitLineClamp: 1,
-        overflow: 'hidden',
-        color: '#333741',
-        textAlign: 'center',
-        textOverflow: 'ellipsis',
-        fontFamily: 'Inter',
-        fontSize: '24px',
-        fontStyle: 'normal',
-        fontWeight: 600,
-        lineHeight: '32px'
-    }
-};
-
-//FUNCTIONALITY
 function SectionHeader({ headerTitle, icon }) {
     return (
-        <div style={sectionHeaderStyles.container}>
-            <img src={icon} style={sectionHeaderStyles.image} />
-            <div style={sectionHeaderStyles.text}>
+        <div className={styles.container}>
+            <img src={icon} className={styles.image} alt="icon"/>
+            <div className={styles.text}>
                 {headerTitle}
             </div>
         </div>
     );
-};
+}
 
 export default SectionHeader;
