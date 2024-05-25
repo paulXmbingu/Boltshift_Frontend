0775492320284635ca1d5cb4871f271b3f96c836
Paul <paulXmbingu@gmail.com>
2024-05-26 00:15:33 +0300

    Update

--- src/Common/FavoriteButton.jsx
+++ src/Common/FavoriteButton.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./FavoriteButton.module.css";
 import WishilistIcon from "../assets/Icons/UI/heart-rounded.svg";
+import WishilistIconFilled from "../assets/Icons/UI/heart-rounded-filled.svg";
 
 function FavoriteButton() {
+    const [isFilled, setIsFilled] = useState(false);
+
+    const handleClick = () => {
+        setIsFilled(!isFilled);
+    };
+
     return (
-        <div className={styles.favStyling}>
+        <div className={styles.favStyling} onClick={handleClick}>
             <div className={styles.featuredIcon}>
-                <img src={WishilistIcon} alt="Wishlist Icon"/>
+                <img
+                    src={isFilled ? WishilistIconFilled : WishilistIcon}
+                    alt="Wishlist Icon"
+                    className={styles.wishilistIcon}
+                />
             </div>
         </div>
     );
