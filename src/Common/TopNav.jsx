9b8ced2ca5e727d97b2cc9314b2893b41b54f0c2
Paul <paulXmbingu@gmail.com>
2024-05-28 08:39:08 +0300

    Update

--- src/Common/TopNav.jsx
+++ src/Common/TopNav.jsx
@@ -1,8 +1,8 @@
 import React from "react";
+import styles from './TopNav.module.css';
 import ProductLogo from "../assets/Logos/Brand Logo.svg";
 import MenuIcon from "../assets/Icons/UI/menu-03.svg";
 import UserAvatar from "../Components/Atoms/UserAvatar";
-import styles from './TopNav.module.css';
 import UserPhoto from '../assets/Images/Photos/Paul Mbingu - Avatar.png';
 import NotificationIcon from "../Components/Molecules/NotificationIcon";
 import Favorite from '../assets/Icons/UI/heart.svg'
