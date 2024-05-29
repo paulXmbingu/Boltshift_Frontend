2b6d2a5c7ec96a2fbe09ba4e6a672b1cd1d530e9
Paul <paulXmbingu@gmail.com>
2024-05-29 15:34:34 +0300

    Update

--- src/Components/Molecules/SearchInput.jsx
+++ src/Components/Molecules/SearchInput.jsx
@@ -4,7 +4,14 @@ import styles from './SearchInput.module.css';
 const SearchInput = () => {
     return(
         <div className={styles.searchInput}>
-            Search Input
+            <div className={styles.inputWithLabel}>
+                <input
+                    type="search" 
+                    name="" 
+                    id="" 
+                    className={styles.input}
+                />
+            </div>
         </div>
     );
 };
