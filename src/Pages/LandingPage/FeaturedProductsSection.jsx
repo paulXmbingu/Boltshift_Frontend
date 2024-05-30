017bd71639e9cc18e67da187e2775f9518bd5221
Paul <paulXmbingu@gmail.com>
2024-05-30 03:48:19 +0300

    Update

--- src/Pages/LandingPage/FeaturedProductsSection.jsx
+++ src/Pages/LandingPage/FeaturedProductsSection.jsx
@@ -11,7 +11,7 @@ function FeaturedProductsSection() {
             <SectionHeader headerTitle="Featured Products" icon={Clipboard}/>
             <div className={styles.catCards}>
                 <FeaturedProductCard productPhoto={`https://i.pinimg.com/736x/c9/bf/89/c9bf89e3f5006199cd0ef01073569912.jpg`} />
-                <FeaturedProductCard productPhoto={`https://i.pinimg.com/564x/96/2a/e1/962ae1f0ca5b1156fb7a412986f179f9.jpg`} />
+                <FeaturedProductCard productPhoto={`https://i.pinimg.com/564x/d2/76/f6/d276f69a67a26c3f2b4ba446b736a04c.jpg`} />
                     <HotDealCard imageSrc = {`https://i.pinimg.com/736x/eb/c6/9f/ebc69f2a0277643a83694ae8d50030f5.jpg`} productNameText={`Modern White Velvet 3-Seater Sofa Channel Tufted Upholstered Luxury Solid Wood`} ammount={`92,372`} noSold={`67`} />
                     <HotDealCard imageSrc = {`https://i.pinimg.com/564x/3a/bf/06/3abf0692b4e786fcfbc87efdb229f1f9.jpg`} productNameText={`Contemporary Accent Table Nightstand Drawer Storage Bedside Cabinet - 16"L x 16"W x 18"H Off-White 2 Piece Set Nightstands`} ammount={`54,436`} noSold={`28`} />
                     <HotDealCard imageSrc = {`https://i.pinimg.com/564x/8c/90/e2/8c90e2c348da08b9c9cb38d2b5049e8d.jpg`} productNameText={`Candy Color Pointed Toe Low Cut Stiletto High Heels Prom Shoes`}ammount={`56,536`} noSold={`56`} />
