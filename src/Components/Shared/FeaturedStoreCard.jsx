3ad456c306cc37c8e947d1ca947f963f235e1dde
Paul <paulXmbingu@gmail.com>
2024-06-06 05:55:14 +0300

    Update

---
+++ src/Components/Shared/FeaturedStoreCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./FeaturedStoreCard.module.css"
+
+const FeaturedStoreCard = () => {
+    return(
+        <div className={styles.defaultWrapper}>
+            <div className={styles.storeInfo}> Store Info </div>
+        </div>
+    );
+};
+
+export default FeaturedStoreCard;
+
