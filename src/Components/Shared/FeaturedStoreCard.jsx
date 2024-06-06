af6d7034a9b36e15a88058b4b8ac1d141e99fced
Paul <paulXmbingu@gmail.com>
2024-06-06 07:20:03 +0300

    Update

--- src/Components/Shared/FeaturedStoreCard.jsx
+++ src/Components/Shared/FeaturedStoreCard.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import styles from "./FeaturedStoreCard.module.css"
 
-const FeaturedStoreCard = ({ storeIcon,  }) => {
+const FeaturedStoreCard = ({ storeIcon, storeDescription }) => {
     return(
         <div className={styles.defaultWrapper}>
             <div className={styles.storeInfo}>
                 <div className={styles.storeDetails}>
                     <img src={storeIcon} alt="Store Icon" className={styles.storeIcon} />
                     <div> Store Name </div>
-                    <div> Store Description </div>
+                    <div className={styles.storeDescription}> {storeDescription} </div>
                 </div>
                 <div> CTA Button </div>
             </div>
