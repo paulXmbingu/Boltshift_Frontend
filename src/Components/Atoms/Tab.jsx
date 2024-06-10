388c2e521b54288893448bf25e2c6e345df68f33
Paul <paulXmbingu@gmail.com>
2024-06-10 03:51:01 +0300

    Update

---
+++ src/Components/Atoms/Tab.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./Tab.module.css"
+
+const HorTabUnderLineSM = ({ tabLabel }) => {
+    return (
+        <div className={styles.tabWrap}>
+            <span className={styles.tabLabel}> {tabLabel} </span>
+        </div>
+    );
+};
+
+const HorTabUnderLineSMSelected = ({ tabLabel }) => {
+    return (
+        <div className={styles.tabWrapSelected}>
+            <span className={styles.tabLabelSelected}> {tabLabel} </span>
+        </div>
+    );
+};
+
+export { HorTabUnderLineSM, HorTabUnderLineSMSelected }
