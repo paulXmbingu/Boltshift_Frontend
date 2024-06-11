44349e422605aed89739889126ea5516a5f3c278
Paul <paulXmbingu@gmail.com>
2024-06-12 01:59:35 +0300

    Update

--- src/Pages/LandingPage/HeroSection.jsx
+++ src/Pages/LandingPage/HeroSection.jsx
@@ -1,13 +1,27 @@
-import React from "react";
-import HeroCard from "../../Components/Shared/HeroCard";
+import React, { useState, useEffect } from "react";
+import { HeroCardDesktop, HeroCardMobile } from "../../Components/Shared/HeroCard";
 import styles from "./HeroSection.module.css";
 
-function HeroSection() {
-    return (
-        <div className={styles.heroSection}>
-            <HeroCard />
-        </div>
-    );
+const useIsLargeScreen = () => {
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth > 768);
+
+  useEffect(() => {
+    const handleResize = () => setIsLargeScreen(window.innerWidth > 768);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return isLargeScreen;
 };
+
+function HeroSection() {
+  const isLargeScreen = useIsLargeScreen();
+
+  return (
+    <div className={styles.heroSection}>
+      {isLargeScreen ? <HeroCardDesktop /> : <HeroCardMobile />}
+    </div>
+  );
+}
 
 export default HeroSection;
