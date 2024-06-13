198dc2e06b74c09b4d57543739dfc31d33b21312
Paul <paulXmbingu@gmail.com>
2024-06-13 19:05:29 +0300

    Update

--- src/Components/Shared/ReviewCard.jsx
+++ src/Components/Shared/ReviewCard.jsx
@@ -3,28 +3,42 @@ import styles from "./ReviewCard.module.css";
 import { Badge_PillColor_SM_LeadIcon_Success } from "../Atoms/Badge";
 import CheckIcon from "../../assets/Icons/UI/check.svg";
 
-const ReviewCard = ({ ReviewerName, ReviewerPhoto,ReviewDate, Review }) => {
+const ReviewCard = ({ ReviewerName, ReviewerPhoto,ReviewDate, Review, ProductImage, ProductNameLabel, PriceLabel }) => {
     return (
         <div className={styles.cardWrap}>
-            <div className={styles.buyerDetails}>
-                <div className={styles.buyerPhoto}> 
-                    <img src={ReviewerPhoto} alt="" className={styles.buyerAvatar} />
-                </div>
-                <div className={styles.detailsStack}> 
-                    <div className={styles.buyerNameVerification}>
-                        <div className={styles.buyerName}> {ReviewerName} </div>
-                        <Badge_PillColor_SM_LeadIcon_Success LeadingIcon={CheckIcon} Label={`Verified Buyer`} />
+            <div className={styles.reviewDetails}>      
+                <div className={styles.buyerDetails}>
+                    <div className={styles.buyerPhoto}> 
+                        <img src={ReviewerPhoto} alt="" className={styles.buyerAvatar} />
                     </div>
-                    <div> 
-                        <div className={styles.dateWrap}> 
-                            <span className={styles.date}> {ReviewDate} </span>
+                    <div className={styles.detailsStack}> 
+                        <div className={styles.buyerNameVerification}>
+                            <div className={styles.buyerName}> {ReviewerName} </div>
+                            <Badge_PillColor_SM_LeadIcon_Success LeadingIcon={CheckIcon} Label={`Verified Buyer`} />
                         </div>
-                        <div> ⭐⭐⭐⭐☆ </div>
+                        <div> 
+                            <div className={styles.dateWrap}> 
+                                <span className={styles.date}> {ReviewDate} </span>
+                            </div>
+                            <div> ⭐⭐⭐⭐☆ </div>
+                        </div>
                     </div>
+                </div>
+                <div className={styles.testimonialText}>
+                    {Review}
                 </div>
             </div>
-            <div className={styles.testimonialText}>
-                {Review}
+            <div className={styles.reviewedProduct}>
+                <img src={ProductImage} alt="Product Image" className={styles.productImage}/>
+                <div className={styles.productNamePrice}>
+                    <div className={styles.productNameWrap}> 
+                        <div className={styles.productNameLabel}> {ProductNameLabel} </div>
+                    </div>
+                    <div className={styles.productPriceWrap}>
+                        <div className={styles.currency}> Ksh. </div>
+                        <div className={styles.priceLabel}> {PriceLabel} </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
