0f75a365ae32b61995285e4946ab00cf108a306c
Paul <paulXmbingu@gmail.com>
2024-06-17 16:09:44 +0300

    Update

--- src/Pages/LandingPage/FeaturedStoreSection.jsx
+++ src/Pages/LandingPage/FeaturedStoreSection.jsx
@@ -17,13 +17,13 @@ function FeaturedStoreSection() {
                 <div> Horizontal Tabs </div>
                 <div className={styles.smallLandscapeCards}>
                     <ProductCardLandscapeSM price={`104,899`} productLabel={`Recliner Lounge Chairs - Relax in Style and Comfort - Crafted from Genuine Leather`} ProductImage={`https://i.pinimg.com/564x/cc/63/67/cc6367600b3b55d912e60a24076d4c9d.jpg`}  />
-                    <ProductCardLandscapeSM price={`104,899`} productLabel={`Recliner Lounge Chairs - Relax in Style and Comfort - Crafted from Genuine Leather`} ProductImage={`https://i.pinimg.com/564x/cc/63/67/cc6367600b3b55d912e60a24076d4c9d.jpg`}  />
-                    <ProductCardLandscapeSM price={`104,899`} productLabel={`Recliner Lounge Chairs - Relax in Style and Comfort - Crafted from Genuine Leather`} ProductImage={`https://i.pinimg.com/564x/cc/63/67/cc6367600b3b55d912e60a24076d4c9d.jpg`}  />
-                    <ProductCardLandscapeSM price={`104,899`} productLabel={`Recliner Lounge Chairs - Relax in Style and Comfort - Crafted from Genuine Leather`} ProductImage={`https://i.pinimg.com/564x/cc/63/67/cc6367600b3b55d912e60a24076d4c9d.jpg`}  />
-                    <ProductCardLandscapeSM price={`104,899`} productLabel={`Recliner Lounge Chairs - Relax in Style and Comfort - Crafted from Genuine Leather`} ProductImage={`https://i.pinimg.com/564x/cc/63/67/cc6367600b3b55d912e60a24076d4c9d.jpg`}  />
-                    <ProductCardLandscapeSM price={`104,899`} productLabel={`Recliner Lounge Chairs - Relax in Style and Comfort - Crafted from Genuine Leather`} ProductImage={`https://i.pinimg.com/564x/cc/63/67/cc6367600b3b55d912e60a24076d4c9d.jpg`}  />
-                    <ProductCardLandscapeSM price={`104,899`} productLabel={`Recliner Lounge Chairs - Relax in Style and Comfort - Crafted from Genuine Leather`} ProductImage={`https://i.pinimg.com/564x/cc/63/67/cc6367600b3b55d912e60a24076d4c9d.jpg`}  />
-                    <ProductCardLandscapeSM price={`104,899`} productLabel={`Recliner Lounge Chairs - Relax in Style and Comfort - Crafted from Genuine Leather`} ProductImage={`https://i.pinimg.com/564x/cc/63/67/cc6367600b3b55d912e60a24076d4c9d.jpg`}  />
+                    <ProductCardLandscapeSM price={`32,078`} productLabel={`Men's PU Shoulder Bag with Zipper Closure`} ProductImage={`https://i.pinimg.com/736x/39/14/15/391415b615904b2d0b09d1f1edbe9a98.jpg`}  />
+                    <ProductCardLandscapeSM price={`29,499`} productLabel={`Versace Accessories | New Ve2269 1002/87 Versace Gold Mod 2269 1002/87 Unisex Sunglasses Ve2269 100287 | Color: Gold/Gray | Size: Os`} ProductImage={`https://i.pinimg.com/564x/be/36/98/be3698f899e58aa9c0e724b8f832ade2.jpg`}  />
+                    <ProductCardLandscapeSM price={`19,990`} productLabel={`Yoetey Mens Belt, Leather Belt Reversible Belt for Men, Double Style Singular Elegance 1 3/8" (35mm), Men's, Size:40" (Fits Pants Size 38"), Multicolor`} ProductImage={`https://i.pinimg.com/736x/38/a3/35/38a33564ea28c293db49727b39264223.jpg`}  />
+                    <ProductCardLandscapeSM price={`25,987`} productLabel={`Pacific Coast Lighting Vincent Nickel Crystal Console Table Lamps Set of 2`} ProductImage={`https://i.pinimg.com/736x/eb/ac/8e/ebac8efef4c90dd691fb286c365b0211.jpg`}  />
+                    <ProductCardLandscapeSM price={`4,895`} productLabel={`Ettika Set of 3 Stretch Cuffs in Gold at Nordstrom`} ProductImage={`https://i.pinimg.com/474x/78/9e/13/789e13125b4d46ba683d4d1e73063645.jpg`}  />
+                    <ProductCardLandscapeSM price={`3,100`} productLabel={`Single Shoulder Strap Open-Back Sports Bra Brown XL`} ProductImage={`https://i.pinimg.com/736x/c7/0c/d5/c70cd58357fb4568a8d912ff520f787d.jpg`}  />
+                    <ProductCardLandscapeSM price={`7,172`} productLabel={`13*4 Lace Front Wigs Human Hair Wig Front Lace Wig Human Hair Wigs Pre Plucked Remy Hair Wig 180 Density Human Hair Wig For Women / 180Density-13*4`} ProductImage={`https://i.pinimg.com/564x/93/70/6b/93706b357c37899781c86fad487107ad.jpg`}  />
                 </div>
                 <div> Pagination </div>
             </div>
