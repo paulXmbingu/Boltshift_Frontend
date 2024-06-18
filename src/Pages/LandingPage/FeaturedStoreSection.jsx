10ca5f9ea71c6bb06ce44055d04e5b9d37c2d09a
Paul <paulXmbingu@gmail.com>
2024-06-18 14:09:21 +0300

    Update

--- src/Pages/LandingPage/FeaturedStoreSection.jsx
+++ src/Pages/LandingPage/FeaturedStoreSection.jsx
@@ -3,6 +3,7 @@ import styles from "./FeaturedStoreSection.module.css";
 import SectionHeader from "../../Components/Shared/SectionHeader";
 import ConvenienceStore from "../../assets/Icons/Illustrated/Convenience store.svg";
 import ProductCardLandscapeSM from "../../Components/Shared/ProductCardLandscapeSM"
+import StoreLogo from "../../assets/Logos/Senjes Cuisines.svg"
 
 function FeaturedStoreSection() {
     return (
@@ -12,8 +13,14 @@ function FeaturedStoreSection() {
                 <div className={styles.storeBackgoundWrapper}>
                     <img src={`https://res.cloudinary.com/excit3/image/upload/v1718663336/Boltshift%20Branding/Store_Background_y8a6x7.jpg`} alt="Store Background" className={styles.storeBackgound} /> 
                 </div>
-                <div> Store Cover Art </div>
-                <div> Store Details </div>
+                <div className={styles.storeDetails}> 
+                    <img src={StoreLogo} alt="Store Logo" className={styles.storeLogo} /> 
+                    <div>Store Name</div>
+                    <div>CTAs</div>
+                    <div className={styles.storeDescription}>
+                        Senjes Cuisines is a renowned eatery offering a fusion of global flavors, emphasizing local ingredients and culinary traditions. Our diverse menu features gourmet dishes, artisanal beverages, and exquisite desserts, all crafted with passion and precision. Senjes Cuisines is dedicated to providing an exceptional dining experience in a cozy and welcoming atmosphere.
+                    </div>
+                </div>
                 <div> Horizontal Tabs </div>
                 <div className={styles.smallLandscapeCards}>
                     <ProductCardLandscapeSM price={`104,899`} productLabel={`Recliner Lounge Chairs - Relax in Style and Comfort - Crafted from Genuine Leather`} ProductImage={`https://i.pinimg.com/564x/cc/63/67/cc6367600b3b55d912e60a24076d4c9d.jpg`}  />
