5db5f88d07cab5761191bc4d9f0f1dd1b5371869
Paul <paulXmbingu@gmail.com>
2024-06-23 01:43:50 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
-import React, { useEffect } from 'react';
-import './App.css';
-import LandingPage from './Pages/LandingPage/LandingPage';
+import './App.css'
+import LandingPage from './Pages/LandingPage/LandingPage'
 
 function App() {
-  useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
-          .then(registration => {
-            console.log('Service Worker registered with scope:', registration.scope);
-          })
-          .catch(error => {
-            console.error('Service Worker registration failed:', error);
-          });
-      });
-    }
-  }, []);
-
   return (
     <div>
       <LandingPage />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
