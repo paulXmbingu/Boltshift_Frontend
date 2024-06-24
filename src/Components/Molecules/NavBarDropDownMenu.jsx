ebf9d9b63185f4c44d497b647dbb9bdac7c06e50
Paul <paulXmbingu@gmail.com>
2024-06-25 01:57:50 +0300

    Update

---
+++ src/Components/Molecules/NavBarDropDownMenu.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./NavBarDropDownMenu.module.css"
+import { MenuListItem } from "../Atoms/MenuListItem";
+import PersonalInfo from "../../assets/Icons/UI/user-01.svg"
+import Order from "../../assets/Icons/UI/shopping-bag-02.svg"
+import Payment from "../../assets/Icons/UI/shield-dollar.svg"
+import Voucher from "../../assets/Icons/UI/receipt-check.svg"
+import LogOut from "../../assets/Icons/UI/log-out-01.svg"
+
+const NavBarDropDownMenu = () => {
+    return (
+        <div className={styles.navBarDropDownMenuWrap}>
+            
+            <div className={styles.divider}></div>
+            <MenuListItem ActionLabel={`Personal Info`} MenuIcon={PersonalInfo} />
+            <MenuListItem ActionLabel={`Order`} MenuIcon={Order} />
+            <MenuListItem ActionLabel={`Payment`} MenuIcon={Payment} />
+            <MenuListItem ActionLabel={`Vouchers`} MenuIcon={Voucher} />
+            <div className={styles.divider}></div>
+            <MenuListItem ActionLabel={`Log Out`} MenuIcon={LogOut} />
+        </div>
+    )
+}
+
+export { NavBarDropDownMenu }
