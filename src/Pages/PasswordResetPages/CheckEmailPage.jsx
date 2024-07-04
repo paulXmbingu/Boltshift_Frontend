a04c159b1579598c42474f9e33e43c25e7d7d7d4
Paul <paulXmbingu@gmail.com>
2024-07-04 03:19:24 +0300

    Added links to prorduct cards

--- src/Pages/PasswordResetPages/CheckEmailPage.jsx
+++ src/Pages/PasswordResetPages/CheckEmailPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import styles from "./CheckEmailPage.module.css";
 import { TopNavLarge, TopNavSmall } from "../../Components/Shared/TopNav";
-import FooterSection from "../../Components/Shared/Footer";
+import Footer from "../../Components/Shared/Footer";
 
 function CheckEmailPage() {
     const [isLargeScreen, setIsLargeScreen] = useState(typeof window !== "undefined" ? window.innerWidth > 768 : false);
@@ -19,7 +19,7 @@ function CheckEmailPage() {
         <div className={styles.pageLayout}>
             {isLargeScreen ? <TopNavLarge /> : <TopNavSmall />}
             <div> Check Email Page </div>
-            <FooterSection />
+            <Footer />
         </div>
     );
 }
