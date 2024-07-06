fc99346da627b28e034c25ce4e2041c0bcc6c6a0
Paul <paulXmbingu@gmail.com>
2024-07-07 02:21:59 +0300

    Update

--- src/Components/Molecules/NavBarDropDownMenu.jsx
+++ src/Components/Molecules/NavBarDropDownMenu.jsx
@@ -23,12 +23,22 @@ const NavBarDropDownMenu = () => {
     )
 }
 
-const NavBarDropDownMenuMobile = () => {
+const NavBarDropDownMenuMobileLoggedIn = () => {
     return (
         <div className={styles.navBarDropDownMenuMobileWrap}>
-            Mobile Menu
+            <div className={styles.mainMenu}>
+                <div> Nav Header </div>
+                <div className={styles.divider}></div>
+                <div className={styles.menuItems}>
+                    <Link to={`/personalInfo`} className={styles.links} > <MenuListItem ActionLabel={`Personal Info`} MenuIcon={PersonalInfo} /> </Link>
+                    <Link to={`/orders`} className={styles.links} > <MenuListItem ActionLabel={`Orders`} MenuIcon={Order} /> </Link>
+                    <Link to={`/payments`} className={styles.links} > <MenuListItem ActionLabel={`Payment`} MenuIcon={Payment} /> </Link>
+                    <Link to={`/vouchers`} className={styles.links} > <MenuListItem ActionLabel={`Vouchers`} MenuIcon={Voucher} /> </Link>
+                </div>
+            </div>
+            <MenuListItem ActionLabel={`Log Out`} MenuIcon={LogOut} classname={styles.logout} />
         </div>
     );
 };
 
-export { NavBarDropDownMenu, NavBarDropDownMenuMobile }
+export { NavBarDropDownMenu, NavBarDropDownMenuMobileLoggedIn }
