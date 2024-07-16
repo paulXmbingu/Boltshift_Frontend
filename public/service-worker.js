c0e39e65beab7c4d0d5776932819f4ad26f04b84
Paul <paulXmbingu@gmail.com>
2024-07-16 12:45:00 +0300

    Update

--- public/service-worker.js
+++ public/service-worker.js
@@ -47,25 +47,25 @@ self.addEventListener('activate', event => {
   );
 });
 
-// // Network Fallback
-// self.addEventListener('fetch', event => {
-//   event.respondWith(
-//     caches.match(event.request).then(response => {
-//       return response || fetch(event.request).catch(() => {
-//         return caches.match('/offline.html'); // Serve offline page if network fails
-//       });
-//     })
-//   );
-// });
+// Network Fallback
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => {
+        return caches.match('/offline.html'); // Serve offline page if network fails
+      });
+    })
+  );
+});
 
-// // Logging Errors
-// self.addEventListener('install', event => {
-//   event.waitUntil(
-//     caches.open(CACHE_NAME).then(cache => {
-//       console.log('Opened cache');
-//       return cache.addAll(urlsToCache);
-//     }).catch(error => {
-//       console.error('Cache open failed:', error);
-//     })
-//   );
-// });
+// Logging Errors
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    }).catch(error => {
+      console.error('Cache open failed:', error);
+    })
+  );
+});
