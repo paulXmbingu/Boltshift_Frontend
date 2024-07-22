be15dfcd21968c2aca6fb1615489da4a208fc8bb
Paul <paulXmbingu@gmail.com>
2024-07-23 00:10:35 +0300

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -11,14 +11,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </React.StrictMode>,
   </BrowserRouter>
 )
-
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker.register('/service-worker.js')
-//       .then(registration => {
-//         console.log('ServiceWorker registration successful with scope: ', registration.scope);
-//       }, err => {
-//         console.log('ServiceWorker registration failed: ', err);
-//       });
-//   });
-// }
