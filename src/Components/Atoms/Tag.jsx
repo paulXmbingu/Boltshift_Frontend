cd7b16aaa2944020e3a875e3f1d26a6ccd3efdb5
Paul <paulXmbingu@gmail.com>
2024-08-08 14:56:43 +0300

    Update

--- src/Components/Atoms/Tag.jsx
+++ src/Components/Atoms/Tag.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import styles from "./Tag.module.css"
 
-export default function TagCheckboxSM( { Label }) {
+export default function TagCheckboxSM( { Label, checked, onChange }) {
     return (
         <div className={styles.TagCheckboxSM}>
             <input
                 type="radio" 
-                name="" 
-                id=""
+                name="tag" 
+                id="tagID"
+                checked={checked}
+                onChange={onChange}
             />
             <div className={styles.labelSM}> {Label} </div>
         </div>
