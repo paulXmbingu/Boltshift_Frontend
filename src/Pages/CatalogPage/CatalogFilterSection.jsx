9cc7f8fd54ad1dee578f55f66af506a3eb1f2868
Felix-ouma <felixouma859@gmail.com>
2024-08-15 12:56:17 +0300

    Update

--- src/Pages/CatalogPage/CatalogFilterSection.jsx
+++ src/Pages/CatalogPage/CatalogFilterSection.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import styles from "./CatalogFilterSection.module.css"
 import CatalogCartegoriesFilter from './CatalogCartegoriesFilter'
-import FilterDropDown from '../../Components/Atoms/FilterDropDown'
 
 const CatalogFilterSection = () => {
   return (
     <div className={styles.CatalogFilterSection}>
-        <FilterDropDown />
+        <div className={styles.imputField}>
+            <form action="GET" className={styles.ImputWithLabel}>
+                <label htmlFor="SortBy" className={styles.SortBy}>Sort By</label>
+                <select id="SortBy" name="SortBy" className={styles.Sortings}>
+                    <option value="Option1"className={styles.optionRext}>Latest</option>
+                    <option value="option2"className={styles.optionRext}>Older</option>
+                    <option value="option3"className={styles.optionRext}>Porpular</option>
+                </select>
+            </form>
+        </div>
         <CatalogCartegoriesFilter />
         <div></div>
         <div></div>
