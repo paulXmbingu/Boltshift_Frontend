9ba825f19e721f2638d683f82cdd3a2aa3ccdfe4
Felix-ouma <felixouma859@gmail.com>
2024-08-16 10:23:32 +0300

    Update

--- src/Components/Atoms/FilterDropDown.jsx
+++ src/Components/Atoms/FilterDropDown.jsx
@@ -3,18 +3,16 @@ import styles from "./FilterDropDown.module.css"
 
 const FilterDropDown = () => {
   return (
-    <div>
-        <div className={styles.imputField}>
+        <div className={styles.FilterDropDown}>
             <form action="GET" className={styles.ImputWithLabel}>
-                <label htmlFor="SortBy" className={styles.SortBy}>Sort By</label>
-                <select id="SortBy" name="SortBy" className={styles.Sortings}>
-                    <option value="Option1"className={styles.optionRext}>Latest</option>
-                    <option value="option2"className={styles.optionRext}>Older</option>
-                    <option value="option3"className={styles.optionRext}>Porpular</option>
+                <label htmlFor="SortBy" className={styles.inputLabel}>Sort By</label>
+                <select id="SortBy" name="SortBy" className={styles.InputOptions}>
+                    <option value="Option1" className={styles.optionRext}>Latest</option>
+                    <option value="Option2" className={styles.optionRext}>Older</option>
+                    <option value="Option3" className={styles.optionRext}>Popular</option>
                 </select>
             </form>
         </div>
-    </div>
   )
 }
 
