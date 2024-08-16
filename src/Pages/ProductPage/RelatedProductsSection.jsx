196de8e9b77f4920c3fbe76043a1d984a6454511
Paul <paulXmbingu@gmail.com>
2024-08-16 18:54:39 +0300

    Update

--- src/Pages/ProductPage/RelatedProductsSection.jsx
+++ src/Pages/ProductPage/RelatedProductsSection.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "./RelatedProductsSection.module.css";
-import SectionHeader from "../../Components/Shared/SectionHeader";
 import ProductCardLandscapeSM from "../../Components/Shared/ProductCardLandscapeSM";
 import { SecSectionHeader } from "../../Components/Shared/SecondarySectionHeader";
 
@@ -11,19 +10,19 @@ function RelatedProductsSection () {
             <div className={styles.smallLandscapeCards}>
                 <div className={styles.cardStacking}>
                     <ProductCardLandscapeSM ProductImage={`https://i.pinimg.com/474x/dd/7e/54/dd7e542fd2833ecb911046631c537fd3.jpg`} productLabel={`Fisdy - Chic Patchwork Backless Dress with Contrast Spaghetti Straps and Wrapped Skirt`} price={`24,999`} />
-                    <ProductCardLandscapeSM ProductImage={`https://i.pinimg.com/736x/04/ae/f0/04aef0f7fa822bde22b2255d5da915c6.jpg`} productLabel={`Women's Sandals High Heel Round Toe Square Toe PU Solid Colored Black White`} price={`12,654`} />
+                    <ProductCardLandscapeSM ProductImage={`https://i.pinimg.com/474x/19/bb/e9/19bbe94b17a09eb9625e9ce214af63e1.jpg`} productLabel={`Beige Falling For You Corset`} price={`59,499`} />
                 </div>
                 <div className={styles.cardStacking}>
-                    <ProductCardLandscapeSM ProductImage={`https://i.pinimg.com/736x/9d/76/85/9d76857b04682b6f5dd261e3c9368a4f.jpg`} productLabel={`Modern Orange & Coffee PU Leather Dining Chair (Set of 2) Open Back with Arms`} price={`76,423`} />
-                    <ProductCardLandscapeSM ProductImage={`https://i.pinimg.com/736x/cc/20/53/cc2053f6e58a1315425ef392a49574d3.jpg`} productLabel={`Billionm Women Bras Push Up Brassiere Seamless Underwear Solid Wireless Lingerie One-pieces Gather Convertible Straps Bralette B046-3-dark purple-36-80AB`} price={`69,098`} />
+                    <ProductCardLandscapeSM ProductImage={`https://i.pinimg.com/474x/95/93/d5/9593d5e1bfb85ca8a8aa82dac315439d.jpg`} productLabel={`Tan Missy Cable Sweater Midi Skirt`} price={`34,899`} />
+                    <ProductCardLandscapeSM ProductImage={`https://i.pinimg.com/736x/f7/12/8c/f7128c5cf0844a3733e0b44296925275.jpg`} productLabel={`Grey Refined Belted Leg Pants`} price={`79,399`} />
                 </div>
                 <div className={styles.cardStacking}>
-                    <ProductCardLandscapeSM ProductImage={`https://i.pinimg.com/564x/e9/99/f3/e999f316abc5b399cd30a3c9100ab31f.jpg`} productLabel={`Round Women Gradient Fashion Rimless Metal Sun Glass`} price={`35,921`} />
-                    <ProductCardLandscapeSM ProductImage={`https://i.pinimg.com/564x/3a/a1/0b/3aa10b3eaf5f1f660cd9a203130c65fb.jpg`} productLabel={`Gibson Soho Lounge Square 16-Piece Reactive Glaze Stoneware Dinnerware Set`} price={`42,751`} />
+                    <ProductCardLandscapeSM ProductImage={`https://i.pinimg.com/736x/1f/47/c6/1f47c6f27a0203e97f25efdac31eb379.jpg`} productLabel={`Women Vintage Ruffle Business Colorblock Work Church Pencil Dress`} price={`89,799`} />
+                    <ProductCardLandscapeSM ProductImage={`https://i.pinimg.com/474x/0b/e7/99/0be799869bd5d16323dfedd1b1671622.jpg`} productLabel={`Cut Out Half Sleeve Ruched Slim Fit High Slit White Gowns Maxi Dresses Chartreuse-2XL`} price={`59,999`} />
                 </div>
                 <div className={styles.cardStacking}>
-                    <ProductCardLandscapeSM ProductImage={`https://i.pinimg.com/736x/da/cc/f5/daccf5fbf21bf638a132ae353c4998f9.jpg`} productLabel={`1pc Men Watch Luxury Waterproof Luminous Week Display Stainless Steel Quartz Watch For Men Dating`} price={`21,999`} />
-                    <ProductCardLandscapeSM ProductImage={`https://i.pinimg.com/564x/85/1a/f8/851af8f07ffa4b245043e6ecbb2c0eb2.jpg`} productLabel={`Mercer41 Dashona White Rectangular Sintered Stone Dining Table w/ Double Pedestals Metal in Yellow | 29.5 H x 55 W x 31.5 D in | Wayfair`} price={`89,492`} />
+                    <ProductCardLandscapeSM ProductImage={`https://i.pinimg.com/736x/7e/37/22/7e372206649c84d387bba293458d7938.jpg`} productLabel={`Klara Knit Cardigan - Black/Cream`} price={`31,699`} />
+                    <ProductCardLandscapeSM ProductImage={`https://i.pinimg.com/736x/40/8d/08/408d082aa0724c99690994734b69a9b4.jpg`} productLabel={`Asymmetrical Neck Slit Maxi Dress / XXL(14)`} price={`23,899`} />
                 </div>
             </div>
         </div>
