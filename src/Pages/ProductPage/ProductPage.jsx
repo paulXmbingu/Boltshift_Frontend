06de424f54205f740efc2bcd5f99bf20e7f4ef74
Paul <paulXmbingu@gmail.com>
2024-08-19 14:22:53 +0300

    Update

--- src/Pages/ProductPage/ProductPage.jsx
+++ src/Pages/ProductPage/ProductPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styles from "./ProductPage.module.css";
 import { TopNavLarge, TopNavSmall } from "../../Components/Shared/TopNav";
-import FooterSection from "../../Components/Shared/Footer";
 import ProductMediaConfiguratorSection from "./ProductMediaConfiguratorSection";
 import Footer from "../../Components/Shared/Footer";
 import BreadCrumb from "../../Components/Shared/BreadCrumb";
@@ -9,6 +8,8 @@ import FeaturedStoreSection from "../LandingPage/FeaturedStoreSection";
 import { DescriptionSection } from "./DescriptionSection";
 import { ProductDetailsSection } from "./ProductDetailsSection";
 import RelatedProductsSection from "./RelatedProductsSection";
+import { Pagination } from "../../Components/Molecules/Pagination";
+import { TestimonialCard_LoggedIn } from "../../Components/Shared/TestimonialCard";
 
 function ProductPage() {
     const [isLargeScreen, setIsLargeScreen] = useState(typeof window !== "undefined" ? window.innerWidth > 768 : false);
@@ -29,9 +30,13 @@ function ProductPage() {
             <ProductMediaConfiguratorSection />
             <DescriptionSection />
             <ProductDetailsSection />
+            <div className={styles.ratingsReviewsRecomendationsSection}>
+                <div className={styles.buyerReview}>
+                    <TestimonialCard_LoggedIn />
+                    <Pagination />
+                </div>
                 <RelatedProductsSection />
                 <FeaturedStoreSection HeaderTitle={`More from this Store`} />
-            <div className={styles.ratingsReviewsRecomendationsSection}>
             </div>
             <Footer />
         </div>
