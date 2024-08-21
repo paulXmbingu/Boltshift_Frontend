0a88b6f630a60f4e5696a74cb880f55bd6f2194e
Paul <paulXmbingu@gmail.com>
2024-08-21 20:41:02 +0300

    Update

--- src/Pages/ProductPage/ReviewSummary.jsx
+++ src/Pages/ProductPage/ReviewSummary.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import styles from './ReviewSummary.module.css'
 import { SecSectionHeader } from "../../Components/Shared/SecondarySectionHeader";
+import { Btn_Primary_LG } from "../../Components/Atoms/Button";
 
 const ReviewSummary = () => {
     return (
         <div className={styles.reviewSummaryWrap}>
             <SecSectionHeader headerTitle={`Reviews`} />
             <div className={styles.reviewInfographics}>
-                Review Infographics
+                <div className={styles.ratingInfographic}>
+                    <div> Rating Summary </div>
+                    <div> CTA Button </div>
+                </div>
+                <div className={styles.starsInfographic}> Stars Infographic </div>
             </div>
         </div>
     );
