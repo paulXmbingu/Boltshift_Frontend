7d35cc3fc5462e9bc8735db069b987a31ce6a223
Paul <paulXmbingu@gmail.com>
2024-08-23 00:22:18 +0300

    Update

--- src/Components/Atoms/MisIcons.jsx
+++ src/Components/Atoms/MisIcons.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 import styles from './MisIcons.module.css'
+import Yellow100pcStar from '../../assets/Icons/Illustrated/Star Fill100pc ColorYellow.svg'
 
 //MISCELLANEOUS ICONS
-
 //Star Icons
 
+//Yellow Stars
+
+const Star_Yellow_100pc = ({ width = 20, height = 20 }) => {
+    return (
+        <img
+            src={Yellow100pcStar}
+            alt={`Star Icon`} 
+            style={{ width: `${width}px`, height: `${height}px` }}
+        />
+    );
+};
+
+
+export {
+    Star_Yellow_100pc,
+}
