b8e74411ad52395579deba2a24ac1e0b94742e6d
Paul <paulXmbingu@gmail.com>
2024-08-25 21:51:48 +0300

    Update

--- src/Components/Shared/ReviewModal/ReviewModal.jsx
+++ src/Components/Shared/ReviewModal/ReviewModal.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import styles from './ReviewModal.module.css';
+import { Btn_Primary_MD } from "../../Atoms/Button";
 
 const ReviewModalDesktop = ({ onClose }) => {
     return (
         <div className={styles.modalOverlay} onClick={onClose}>
             <div className={styles.reviewModalDesktopWrap} onClick={(e) => e.stopPropagation()}>
-                Review Modal Desktop
+                <div className={styles.modalHeader}> Modal Header </div>
+                <ReviewDetails />
+                <ModalFooter />
             </div>
+        </div>
+    );
+};
+
+const ReviewDetails = () => {
+    return (
+        <div className={styles.modalHeader}>
+            ReviewDetails
+        </div>
+    );
+};
+
+const ModalFooter = () => {
+    return (
+        <div className={styles.modalFooter}>
+            <Btn_Primary_MD ButtonCTA={`Submit Review`}/>
         </div>
     );
 };
