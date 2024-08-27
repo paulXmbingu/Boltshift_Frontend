98c25c7acd1f432f4c2a7f34ed285351ad1fe5e9
Felix-ouma <felixouma859@gmail.com>
2024-08-27 09:18:37 +0300

    Update

--- src/Pages/CatalogPage/CatalogCartegoriesFilter.jsx
+++ src/Pages/CatalogPage/CatalogCartegoriesFilter.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import styles from "./CatalogCartegoriesFilter.module.css"
+import icon from '../../assets/Icons/UI/Dot.svg'
+import { HeaderLabel } from '../../Components/Atoms/CatalogHeader'
+
 
 
 const CatalogCartegoriesFilter = () => {
   return (
     <div className={styles.CartegoriesSection}>
         <div className={styles.Filters}>
-            <span className={styles.HeaderLabelFilters}>Categories</span>
+            <span className={styles.HeaderLabelFilters}>
+            <HeaderLabel HeaderLabelText={`Categes`} HeaderLabelIcon={icon}/>
+            </span>
         </div>
     </div>
   )
--- src/Pages/CatalogPage/CatalogFilterSection.jsx
+++ src/Pages/CatalogPage/CatalogFilterSection.jsx
@@ -1,20 +1,12 @@
 import React from 'react'
 import styles from "./CatalogFilterSection.module.css"
 import CatalogCartegoriesFilter from './CatalogCartegoriesFilter'
-import FilterDropDown from '../../Components/Atoms/FilterDropDown'
 
 const CatalogFilterSection = () => {
   return (
     <div className={styles.CatalogFilterSection}>
-        <FilterDropDown />
+        
         <CatalogCartegoriesFilter />
-        <div>Hello</div>
-        <div>Hello</div>
-        <div>Hello</div>
-        <div>Hello</div>
-        <div>Hello</div>
-        <div>Hello</div>
-        <div>Hello</div>
     </div>
   )
 }
