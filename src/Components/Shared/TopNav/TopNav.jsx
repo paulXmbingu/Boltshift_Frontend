23fa5ed96d213fe8adfcea7892db0c0c187323f7
Paul <paulXmbingu@gmail.com>
2024-08-28 08:50:36 +0300

    Update

--- src/Components/Shared/TopNav/TopNav.jsx
+++ src/Components/Shared/TopNav/TopNav.jsx
@@ -3,7 +3,6 @@ import styles from './TopNav.module.css';
 import { Link } from "react-router-dom";
 import UserAvatar from "../../Atoms/UserAvatar";
 import NotificationIcon from "../../Molecules/NotificationIcon";
-import UserPhoto from '../../../assets/Images/Photos/Paul Mbingu - Avatar.png';
 import MenuIcon from "../../../assets/Icons/UI/menu-03.svg";
 import Favorite from '../../../assets/Icons/UI/heart.svg';
 import Cart from '../../../assets/Icons/UI/shopping-cart-03.svg';
@@ -91,7 +90,7 @@ const TopNavSmall = () => {
           <Link to={`/cart`}> <NotificationIcon icon={Cart} className={styles.notificationsMobileIcon} /> </Link>
           <NotificationIcon icon={Bell} className={styles.notificationsMobileIcon} />
           <div className={styles.notificationAvatar} onClick={handleAvatarClick} ref={dropdownRef}>
-            <UserAvatar userPhoto={UserPhoto} />
+            <UserAvatar userPhoto={`https://res.cloudinary.com/excit3/image/upload/v1718726412/Boltshift%20Branding/Paul_Mbingu_-_Avatar_-_Casual_-_Full_BG_tbr4ml.png`} />
             {showDropDown && (
               <NavBarDropDownMenuMobileLoggedIn className={`${styles.navBarDropDownMenuMobileLoggedIn} ${styles.show}`} />
             )}
