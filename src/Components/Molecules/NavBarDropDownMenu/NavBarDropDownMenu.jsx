c90aa85e17a087cb7229ee87ae5ed6febbc4408a
Paul <paulXmbingu@gmail.com>
2024-08-28 09:26:36 +0300

    Update

--- src/Components/Molecules/NavBarDropDownMenu/NavBarDropDownMenu.jsx
+++ src/Components/Molecules/NavBarDropDownMenu/NavBarDropDownMenu.jsx
@@ -32,32 +32,30 @@ const NavBarDropDownMenu = () => {
     );
 };
 
-const NavBarDropDownMenuMobileLoggedIn = ({ onClose }) => {
+const NavBarDropDownMenuMobileLoggedIn = () => {
     const handleClick = (event) => {
-        event.stopPropagation(); // Prevent closing the menu when clicking inside it
+        event.stopPropagation();
     };
 
     return (
-        <div className={styles.navBarDropDownMenuOverlay} onClick={onClose}>
-            <div className={styles.navBarDropDownMenuMobileWrap} onClick={handleClick}>
-                <div className={styles.mainMenu}>
-                    <DropdownHeaderLoggedinMobile UserName="Paul Mbingu" Email="paul@excite.company" />
-                    <div className={styles.divider}></div>
-                    <div className={styles.menuItems}>
-                        <Link to="/personalInfo" className={styles.links}>
-                            <MenuListItem ActionLabel="Personal Info" MenuIcon={PersonalInfo} />
-                        </Link>
-                        <Link to="/orders" className={styles.links}>
-                            <MenuListItem ActionLabel="Orders" MenuIcon={Order} />
-                        </Link>
-                        <Link to="/payments" className={styles.links}>
-                            <MenuListItem ActionLabel="Payments" MenuIcon={Payment} />
-                        </Link>
-                        <Link to="/vouchers" className={styles.links}>
-                            <MenuListItem ActionLabel="Vouchers" MenuIcon={Voucher} />
-                        </Link>
-                        <MenuListItem ActionLabel="Log Out" MenuIcon={LogOut} className={styles.logout} />
-                    </div>
+        <div className={styles.navBarDropDownMenuMobileWrap} onClick={handleClick}>
+            <div className={styles.mainMenu}>
+                <DropdownHeaderLoggedinMobile UserName="Paul Mbingu" Email="paul@excite.company"/>
+                <div className={styles.divider}></div>
+                <div className={styles.menuItems}>
+                    <Link to="/personalInfo" className={styles.links}> 
+                        <MenuListItem ActionLabel="Personal Info" MenuIcon={PersonalInfo} /> 
+                    </Link>
+                    <Link to="/orders" className={styles.links}> 
+                        <MenuListItem ActionLabel="Orders" MenuIcon={Order} /> 
+                    </Link>
+                    <Link to="/payments" className={styles.links}> 
+                        <MenuListItem ActionLabel="Payments" MenuIcon={Payment} /> 
+                    </Link>
+                    <Link to="/vouchers" className={styles.links}> 
+                        <MenuListItem ActionLabel="Vouchers" MenuIcon={Voucher} /> 
+                    </Link>
+                    <MenuListItem ActionLabel="Log Out" MenuIcon={LogOut} classname={styles.logout} />
                 </div>
             </div>
         </div>
